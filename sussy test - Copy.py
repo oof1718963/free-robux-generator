e2a38e71f7edd0789454285cab2d20e202e33e30
oof1718963 <75444840+oof1718963@users.noreply.github.com>
2022-02-01 20:00:11 -0500

    Add files via upload

---
+++ sussy test - Copy.py
@@ -0,0 +1,43 @@
+from tkinter import *
+from tkinter import messagebox
+from tkinter.messagebox import QUESTION
+
+root = Tk()
+root.title("free bobux")
+root.geometry("600x600")
+
+
+def free_bobux():
+
+        messagebox.showinfo("warning", "i hecked your laptop")   
+        messagebox.showinfo("warning", "i hecked your laptop")   
+        messagebox.showinfo("warning", "i hecked your laptop")   
+        messagebox.showinfo("warning", "i hecked your laptop")   
+        messagebox.showinfo("warning", "i hecked your laptop")   
+        messagebox.showinfo("warning", "i hecked your laptop")   
+        messagebox.showinfo("warning", "i hecked your laptop")
+        messagebox.showinfo("warning", "i hecked your laptop")   
+        messagebox.showinfo("warning", "i hecked your laptop")   
+        messagebox.showinfo("warning", "i hecked your laptop")   
+        messagebox.showinfo("warning", "i hecked your laptop")   
+        messagebox.showinfo("warning", "i hecked your laptop")   
+        messagebox.showinfo("warning", "i hecked your laptop")   
+        messagebox.showinfo("warning", "i hecked your laptop")
+        messagebox.showinfo("warning", "i hecked your laptop")   
+        messagebox.showinfo("warning", "i hecked your laptop")   
+        messagebox.showinfo("warning", "i hecked your laptop")   
+        messagebox.showinfo("warning", "i hecked your laptop")   
+        messagebox.showinfo("warning", "i hecked your laptop")   
+        messagebox.showinfo("warning", "i hecked your laptop")   
+        messagebox.showinfo("warning", "i hecked your laptop")
+        messagebox.showinfo("warning", "i hecked your laptop")   
+        messagebox.showinfo("warning", "i hecked your laptop")   
+        messagebox.showinfo("warning", "i hecked your laptop")   
+        messagebox.showinfo("warning", "i hecked your laptop")   
+        messagebox.showinfo("warning", "i hecked your laptop")   
+        messagebox.showinfo("warning", "i hecked your laptop")   
+        messagebox.showinfo("warning", "i hecked your laptop")
+    
+btn = Button(root, text = "tap here for free robux", command = free_bobux
+btn.pack()
+root.mainloop()
